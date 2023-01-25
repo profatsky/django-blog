f6d74833008ab1b3774a5465400ece1a39994b64
Mikhail Profatsky <mixail.rostov04@gmail.com>
2023-01-26 00:30:13 +0300

    subscribers are notified when a post is published

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.contrib import admin
+from django.core.mail import send_mail
 
 from .models import Post, Category
+from mailings.models import Subscriber
 
 
 @admin.register(Post)
@@ -13,6 +16,18 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'published_at'
     readonly_fields = ('created_at', 'updated_at', 'views')
     ordering = ['status', 'published_at', 'views']
+
+    def save_model(self, request, obj, form, change):
+        if (not change or 'status' in form.changed_data) and obj.status == 'published':
+            for subscriber in Subscriber.objects.all():
+                send_mail(
+                    subject='Блог',
+                    message=f'{subscriber.name}, в Блоге вышла новая статья «{obj.title}» '
+                            f'из категории «{obj.category.title}»!',
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[subscriber.email]
+                )
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(Category)
